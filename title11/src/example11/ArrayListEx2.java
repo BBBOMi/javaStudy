aa79e11494c161343ea48398251c3ac63fe4a5a8
BBBOMi <a51022866@gmail.com>
2018-04-24 22:37:35 +0900

    title11_yet

---
+++ title11/src/example11/ArrayListEx2.java
@@ -0,0 +1,29 @@
+package example11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx2 { // p.587
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int LIMIT = 10;
+		String source = "0123456789abcdefghijABCDEFGHIJ!@#$%^&*()zzz";
+		int length = source.length();
+		
+		List list = new ArrayList(length/LIMIT + 10);
+		
+		for(int i=0; i<length; i+=LIMIT) {
+			if(i+LIMIT < length)
+				list.add(source.substring(i, i+LIMIT));
+			else
+				list.add(source.substring(i));
+		}
+		
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+	}
+
+}
